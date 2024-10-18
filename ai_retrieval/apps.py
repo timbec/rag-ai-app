e83aedf269162de56e3274c9e68f04b6d9b2afbf
Tim Beckett <timbeckett@Tims-Mac-mini.home>
2024-10-18 15:59:47 -0400

    Initial commit before pushing to github.

---
+++ ai_retrieval/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AiRetrievalConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ai_retrieval"
---
+++ ai_retrieval/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
